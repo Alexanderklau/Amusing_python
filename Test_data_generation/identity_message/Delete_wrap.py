f1bc92c88b9d9d51ea01b438f0f27f5e4bf9dfd5
Alexanderklau <429095816@qq.com>
2017-10-22 14:41:28 +0800

    身份信息

---
+++ Test_data_generation/identity_message/Delete_wrap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'yemilice_lau'
+
+
+
+f = open("id_code2.txt","a+")
+lines = f.readlines()
+print lines
+# rep_list = map(lambda x: [x, ''][x == '\n'],lines)
+# print rep_list
+# print lines
+# for line in lines:
+#     print line
+    # line.replace("\n","")
--- Test_data_generation/identity_message/Id_card_number.py
+++ Test_data_generation/identity_message/Id_card_number.py
@@ -13,25 +13,55 @@ S = Sum(Ai*Wi) mod 11 ------------- Sum(Ai*Wi) 取11的模。
 """
 from datetime import date
 from datetime import timedelta
+import random
 
 
 def getdistrictcode():
-    with open('districtcode') as file:
+    with open('id_code.txt') as file:
         data = file.read()
     districtlist = data.split('\n')
     global codelist, state, city
     codelist = []
     for node in districtlist:
-        # print node
-        if node[10:11] != ' ':
-            state = node[10:].strip()
-        if node[10:11] == ' ' and node[12:13] != ' ':
-            city = node[12:].strip()
-        if node[10:11] == ' ' and node[12:13] == ' ':
-            district = node[14:].strip()
-            code = node[0:6]
-            codelist.append({"state": state, "city": city, "district": district, "code": code})
+        if node != '':
+            if node[10:11] != ' ':
+                state = node[10:].strip()
+            if node[10:11] == ' ' and node[12:13] != ' ':
+                city = node[12:].strip()
+            if node[10:11] == ' ' and node[12:13] == ' ':
+                district = node[14:].strip()
+                code = node[0:6]
+                codelist.append({"state": state, "city": city, "district": district, "code": code})
+        else:
+            pass
+    return codelist
 
 
-if __name__ == '__main__':
-    print getdistrictcode()
+def gennerator(code=None):
+    id = codelist[random.randint(0, len(codelist))]['code']  # 地区项
+    id = id + str(random.randint(1930, 2013))  # 年份项
+    da = date.today() + timedelta(days=random.randint(1, 366))  # 月份和日期项
+    id = id + da.strftime('%m%d')
+    id = id + str(random.randint(100, 300))  # ，顺序号简单处理
+
+    i = 0
+    count = 0
+    weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]  # 权重项
+    checkcode = {'0': '1', '1': '0', '2': 'X', '3': '9', '4': '8', '5': '7', '6': '6', '7': '5', '8': '5', '9': '3',
+                 '10': '2'}  # 校验码映射
+    for i in range(0, len(id)):
+        count = count + int(id[i]) * weight[i]
+    id = id + checkcode[str(count % 11)]  # 算出校验码
+    return id
+
+
+if __name__ == "__main__":
+    lt = []
+    i = 1
+    s = 0
+    while i <= 10:
+        i = i + 1
+        code_list = getdistrictcode()
+        code = gennerator(code_list)
+        lt.append(code)
+    print lt
