7abc86d6ae3942b4443e3e63e5b2eb35e6924876
Alexanderklau <429095816@qq.com>
2018-01-08 22:59:23 +0800

    fix

--- FIle/files.py
+++ FIle/files.py
@@ -3,16 +3,24 @@ import os, os.path
 import commands
 import logging
 
-Path = r"/home/lau/下载/test/"
+Path = r"/"
 
+
+# #日志设置
+# def file_log():
+#     logging.basicConfig(level=logging.DEBUG,
+#                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+#                         datefmt='%a, %d %b %Y %H:%M:%S',
+#                         filename='myapp.log',
+#                         filemode='w')
 
 #函数递归遍历目录
 def visitDir(arg, dirname, names):
-    print names
     for filepath in names:     
         filename = os.path.join(dirname, filepath)
         filenames = filename_processing(filename)
         read_file(filenames)
+
 
 #文件名的处理，文件名内含有空格，括号，书名号等，不能直接调用dd，在对文件名处理后进行调用
 def filename_processing(filename):
@@ -20,11 +28,18 @@ def filename_processing(filename):
     replace(")", "\\)").replace("<", "\\<").replace(">", "\\>")
     return filename
 
+#读文件
 def read_file(filename):
-    file, output = commands.getstatusoutput('dd if=' + filename)
-    if file == 0:
-        
-        
+    if os.path.isdir(filename):
+        print "It is a dictory!!"
+    elif os.path.isfile(filename):
+        file, output = commands.getstatusoutput('dd if=' + filename)
+        if file == 0:
+            print 'This is helath file!'
+        else:
+            logging.warning("%s:%s" %(filename,output))
+    else:
+        print "it's a special file (socket, FIFO, device file)"
 
 
 if __name__ == "__main__":
