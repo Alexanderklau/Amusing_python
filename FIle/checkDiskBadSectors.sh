c09290b363c93c879410238f7850bd145fb73bcd
Alexanderklau <429095816@qq.com>
2018-01-24 10:22:23 +0800

    fix

--- FIle/checkDiskBadSectors.sh
+++
@@ -1,4 +0,0 @@
-echo "*/1 * * * * root python /etc/checkDiskBadSectors/checkDiskBadSectors.py >> /opt/checkDiskBadSectors/checkDiskBadSectors.log" >> /etc/crontab
-tar xvf ./checkDiskBadSectors.tar.gz -C /opt
-
-
