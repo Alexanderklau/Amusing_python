4615c060e3e251449661158ccfbbcddb055737f2
Alexanderklau <429095816@qq.com>
2018-01-25 17:50:08 +0800

    2018-1-25

---
+++ unit_test/example/test_ep.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from mathfunc import add, minus, multi, divide
+
+
+class TestMathFunc(unittest.TestCase):
+    """Test mathfuc.py"""
+
+    def test_add(self):
+        """Test method add(a, b)"""
+        self.assertEqual(3, add(1, 2))
+        self.assertNotEqual(3, add(2, 2))
+
+    def test_minus(self):
+        """Test method minus(a, b)"""
+        self.assertEqual(1, minus(3, 2))
+
+    def test_multi(self):
+        """Test method multi(a, b)"""
+        self.assertEqual(6, multi(2, 3))
+
+    def test_divide(self):
+        """Test method divide(a, b)"""
+        self.assertEqual(2, divide(6, 3))
+        self.assertEqual(2.5, divide(5, 2))
+
+if __name__ == '__main__':
+    unittest.main()
