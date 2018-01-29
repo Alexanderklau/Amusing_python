f69533a73a710efcab5efefcf075bc2261a16ced
Alexanderklau <429095816@qq.com>
2018-01-29 15:30:26 +0800

    fix

--- File_operation/check_file/rdfile.c
+++ File_operation/check_file/rdfile.c
@@ -1,14 +1,31 @@
-/***gcc -o libpycall.so -shared -fPIC rdfile.c*/
-#include<stdio.h>
-#define F_PATH "/home/lau/下载/2000W/200W-400W.csv"
-char c;
-int rdfile(char *fpath){
-    FILE*fp=NULL;//需要注意
-    fp=fopen(fpath,"r");
-    if(NULL==fp)
-    return -1;//要返回错误代码
-//    while(fscanf(fp,"%c",&c)!=EOF) printf("%c",c); //从文本中读入并在控制台打印出来
-    fclose(fp);
-    fp=NULL;//需要指向空，否则会指向原打开文件地址
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int rd(char *filename)
+{
+    int fd;
+    int size_read;
+    char buffer[800000];
+    fd = open(filename, O_RDONLY);
+    size_read = read(fd, buffer, sizeof(buffer));
+    while (size_read > 0) /*循环读取数据*/
+    {
+        if (size_read < sizeof(buffer))
+        {
+            buffer[size_read] = '\0';
+        }
+//        printf("%s\n", buffer);
+        size_read = read(fd, buffer, sizeof(buffer) - 1);
+    }
+    if (size_read == -1) /*如果出错*/
+    {
+        perror("Error reading");
+        return -1;
+    }
+    close(fd);
     return 0;
 }
+
