548564113f018be153de555b5db30e542e81f62a
Alexanderklau <429095816@qq.com>
2018-04-17 22:26:54 +0800

    fix

--- spider/Spider_project/spider_toor/code.py
+++ spider/Spider_project/spider_toor/code.py
@@ -5,6 +5,7 @@ __author__ = "Yemilice_lau"
 import requests
 import time
 import json
+from lxml import etree
 
 url = "http://b.jowong.com/createimage?Rgb=255|0|0"
 
@@ -25,17 +26,39 @@ with open('code.jpg', 'wb') as f:
 print('请输入验证码:')
 yanzhen = input()
 
-data = "password=######&random={0}&url=%2Fprovider%2Fticket%2Findex.do&usid=######".format((yanzhen))
+data = "password=321654&random={0}&url=%2Fprovider%2Fticket%2Findex.do&usid=scctkj".format((yanzhen))
 
 s.post(login_url, data=data, headers=headers)
 
 while True:
 
-    time.sleep(1)
+    time.sleep(5)
 
-    f = s.get("http://b.jowong.com/provider/ticket/ticketsearch.do?pdno=06001&rzti=2018-05-02&r=54")
+    f = s.get("http://b.jowong.com/provider/ticket/index.do")
 
-    print(f.text)
+    root = etree.HTML(f.content)
+
+    bznote = root.xpath('/html/body/div[5]/form/div/input//@value')
+
+    print(bznote[0])
+
+    # if len(bznote) == 0:
+    #     continue
+    # else:
+    #     bznote1 = bznote[0]
+    #
+    x = s.get("http://b.jowong.com/provider/ticket/ticketsearch.do?pdno=06001&rzti=2018-05-03")
+    #
+    add_message_url = "http://b.jowong.com/team/addTourist.do?" \
+                      "bznote={0}&" \
+                      "credentialstype=01&credentials=650404399405968403&mobile=13288495869".format(bznote[0])
+    #
+    print(add_message_url)
+    #
+    # k = s.get(add_message_url)
+    #
+    # print(k.text)
+
 
 
 
